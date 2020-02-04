4d0f37651cc96f48f322c6205c2e2cbd7ee82fbd
湯浅なつき <yuasanatsuki@yuasanatsuki-no-MacBook-Air.local>
2020-02-04 13:40:48 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+<v-content>
+<v-container>びゅうティファイ
+<v-layout wrap>
+<!-- xsはスマホ、smはタブレット、mdはPC -->
+<v-flex xs12 sm6 md4 text-center my-5><v-btn>押して</v-btn></v-flex>
+<v-flex xs12 sm6 md4 text-center my-5><v-btn>押して</v-btn></v-flex>
+<v-flex xs12 sm6 md4 text-center my-5><v-btn>押して</v-btn></v-flex>
+</v-layout>
+</v-container>
+</v-content>
+</div>
+</template>
+
+
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
