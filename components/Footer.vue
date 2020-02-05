2ca1e65f19e7347559b8bfcc6a6265135abf158b
湯浅なつき <yuasanatsuki@yuasanatsuki-no-MacBook-Air.local>
2020-02-05 11:03:45 +0900

    headerとfooterをコンポーネントわけた

---
+++ components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <v-footer>
+      <div class="flex-grow-1"></div>
+      <div>&copy; 2019 - {{ new Date().getFullYear() }} なーたん</div>
+    </v-footer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
-<div>
-<v-content>
-<v-container>びゅうティファイ
-<v-layout wrap>
-<!-- xsはスマホ、smはタブレット、mdはPC -->
-<v-flex xs12 sm6 md4 text-center my-5><v-btn>押して</v-btn></v-flex>
-<v-flex xs12 sm6 md4 text-center my-5><v-btn>押して</v-btn></v-flex>
-<v-flex xs12 sm6 md4 text-center my-5><v-btn>押して</v-btn></v-flex>
-</v-layout>
-</v-container>
-</v-content>
-</div>
+  <v-content>
+    <v-container>
+    <v-layout wrap>
+      <v-flex xs12 sm6 md4 text-center my-5><v-btn>押して</v-btn></v-flex>
+      <v-flex xs12 sm6 md4 text-center my-5><v-btn >押して</v-btn></v-flex>
+      <v-flex xs12 sm6 md4 text-center my-5><v-btn>押して</v-btn></v-flex>
+    </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 
@@ -19,10 +16,11 @@
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
+
 export default {
   components: {
     Logo,
-    VuetifyLogo
+    VuetifyLogo,
   }
 }
 </script>
